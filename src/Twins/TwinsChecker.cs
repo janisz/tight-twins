aa233401055443b400945571f4150da1b0b31f27
Tomasz Janiszewski <janiszt@gmail.com>
2016-04-29 20:57:53 +0200

    Implement CheckTwins

--- src/Twins/TwinsChecker.cs
+++ src/Twins/TwinsChecker.cs
@@ -12,9 +12,36 @@ namespace Twins
         /// <returns></returns>
         public static bool CheckTwins(IEnumerable<BoardItem> boardItems)
         {
-            //TODO: właściwa implementacja
+            // TODO: Zamienić IEnumerable na IList
+            int count = 0;
+            foreach (var item in boardItems)
+            {
+                count++;
+            }
+            string[] board = new string[count];
+            foreach (var item in boardItems)
+            {
+                board[item.Value] = item?.Color.ToString() ?? "";
+            }
+            return findTwins(System.String.Join("", board)).Length != 0;
+        }
 
-            return false;
+        /// <summary>
+        /// Zwraca najkrótszego istniejącego ciasnego bliżniaka
+        /// </summary>
+        /// <param name="sequence">słowo nad alfabetem</param>
+        /// <returns>Najkrótszy istnijący ciasny bliźniak lub pusty jeśli nie znaleziono</returns>
+        private static string findTwins(string sequence) {
+            for (int subSequenceLength = 1; subSequenceLength <= sequence.Length / 2; subSequenceLength++) {
+                for (int index = 0; index + 2*subSequenceLength <= sequence.Length; index++) {
+                    string a = sequence.Substring(index, subSequenceLength);
+                    string b = sequence.Substring(index+subSequenceLength, subSequenceLength);
+                    if (a == b) {
+                        return a;
+                    }
+                }
+            }
+            return "";
         }
     }
 }
